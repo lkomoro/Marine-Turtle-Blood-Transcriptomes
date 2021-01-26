7a37a00caa52bce313435d42df338f53ff6b6b97
smbanerjee <33838244+smbanerjee@users.noreply.github.com>
2021-01-26 01:13:19 -0800

    remove specific paths and file names

--- scripts/salmon.sh
+++ scripts/salmon.sh
@@ -10,9 +10,7 @@
 #BSUB -oo salmon.log
 
 module load salmon/1.1.0
-index=Tsalmon_index
-#index is same folder as generated with salmon_index
+index=salmon_index
 
-loc=/project/uma_lisa_komoroske/Kenyon/assemblies/lung/trimmed
-salmon quant -i $index --libType A -1 $loc/rDerCor_LG1_S4_R1_SS.fastq  -2 $loc/rDerCor_LG1_S4_R2_SS.fastq -p 8 -o ./salmon_quant --validateMappings
+salmon quant -i $index --libType A -1 sample_R1_SS.fastq  -2 sample_R2_SS.fastq -p 8 -o ./salmon_quant --validateMappings
 
